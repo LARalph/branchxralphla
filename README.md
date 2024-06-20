6160e6550c4c8c8cf4b8a49c72515421354e9e2d
Ralph Lopez <149750968+LARalph@users.noreply.github.com>
2024-06-20 11:13:09 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# branchxralphla
+# Try Branch X Next Corner
+
+
+Try Branch is a link in bio tool built using [Astro]
+
+
+View the [Demo Site](https:///)
---
+++ src/links.js
@@ -0,0 +1,50 @@
+export const mainContent = {
+  imgSrc: `/assets/icon.png`,
+  name: `Ralph Lopez`,
+  tagline: ``,
+};
+
+
+
+export const socialLinks = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/Ralphla_/',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter',
+    url: '',
+  },
+  {
+    title: 'Linkedin',
+    icon: 'mdi:linkedin',
+    url: 'https://www.linkedin.com/in/ralphalopez/',
+  },
+  {
+    title: 'Email',
+    icon: 'mdi:gmail',
+    url: 'href="mailto:Lopezanthonyralph@gmail.com"',
+  }
+]
+
+export const links = [
+  {
+    text: 'Check out Next Corner',
+    url: 'https://forms.gle/j9HrXJbkn6jdMY5K6',
+    featured: true,
+  },
+  {
+    text: 'Persoanl Website',
+    url: '',
+  },
+  {
+    text: 'Schedule a meeting',
+    url: '',
+  },
+  {
+    text: 'Contact',
+    url: '',
+  }
+]
